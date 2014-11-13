5e4137c4089daf05e137a3d3898dc74f9cef27d2
bharath <bbnsumanth>
2014-11-13 16:55:30 +0530

    second commit

--- src/main/scala/Transpose.scala
+++ src/main/scala/Transpose.scala
@@ -1,7 +1,11 @@
 object Transpose{
-def rowToColumnStore(data: RDD[Vector]): RDD[(Int, Vector)]{
+def rowToColumnStore(data: RDD[Vector]): RDD[(Int, Vector)]={
 
-data.map(x => x.toArray).flatMap(x => x.zipWithIndex).map(x => x.swap).groupByKey.map(x => (x._1,x._2.toVector))
+data.map(x => x.toArray)
+    .flatMap(x => x.zipWithIndex)
+    .map(x => x.swap)
+    .groupByKey
+    .map(x => (x._1,x._2.toVector))
 
 }
 
